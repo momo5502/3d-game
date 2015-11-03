250823691a29e042947ca708084c2247a2bb8bf2
Maurice Heumann <MauriceHeumann@gmail.com>
2015-11-03 22:49:04 +0100

    Fixing physics doesn't seem to work ;)

--- js/game/components/physics.js
+++ js/game/components/physics.js
@@ -25,7 +25,7 @@
 
   GAME.physics.update = function()
   {
-    GAME.DATA.world.step(GAME.var.frameDelta / 1000);
+    GAME.DATA.world.step(/*GAME.var.frameDelta / 1000*/ 1 / 60);
     GAME.camera.syncCollider();
   };
 
